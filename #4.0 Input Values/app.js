90f944972c5c1a24488c024a40b41af73b0e5abb
yangwonhee <yangwonhee@kakao.com>
2022-01-18 23:57:07 +0900

    #4.0 Input Values

---
+++ #4.0 Input Values/app.js
@@ -0,0 +1,9 @@
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+function onLoginBtnClick(){
+    console.log("hello", loginInput.value);
+}
+
+loginButton.addEventListener("click", onLoginBtnClick);
