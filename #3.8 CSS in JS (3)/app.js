850268994ff6f18c1816ddaf17b412ea2f842dc8
yangwonhee <yangwonhee@kakao.com>
2022-01-18 23:44:18 +0900

    #3.8 CSS in JS(3)

--- #3.8 CSS in JS (3)/app.js
+++ #3.8 CSS in JS (3)/app.js
@@ -1,8 +1,7 @@
 // #3.7 CSS in JavaScript (2)
+/* toggle code
 
 const h1 = document.querySelector(".hello:first-child h1");
-
-console.dir(h1);
 
 function handleh1Click(){
     const clickedClass = "clicked"
@@ -11,6 +10,15 @@ function handleh1Click(){
     }else {
         h1.classList.add(clickedClass);
     }
+}
+
+h1.addEventListener("click", handleh1Click);
+*/
+
+const h1 = document.querySelector(".hello:first-child h1");
+
+function handleh1Click(){
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleh1Click);
