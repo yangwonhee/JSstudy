f27270720e6c1310e2b97b97b7315e03f0f7e8ce
yangwonhee <yangwonhee@kakao.com>
2022-01-24 19:22:23 +0900

    #8.0 Geolocation

---
+++ #8.0 Geolocation/js/weather.js
@@ -0,0 +1,13 @@
+function onGeoSuccess(position){
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    console.log("You live in", lat, lng);
+}
+
+function onGeoError(){
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError);
+
+// https://openweathermap.org/api
